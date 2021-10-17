e3538b816f1750c6e0218701777ecfc3b38a7537
EGKangaroo <EGKangaroo@yahoo.com>
2021-10-17 20:20:06 +0200

    convert to string

--- GhostService-API/Models/EvidenceType.cs
+++ GhostService-API/Models/EvidenceType.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GhostService_API.Models
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum EvidenceType
     {
         primary,
